984ddbc8c3eb10ec6a363d485593acb6b6532343
maranedaex <maranedaex@gmail.com>
2020-03-23 10:07:14 -0300

    primera version

---
+++ src/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/notes-db-app',{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(db => console.log('DB is connected'))
+.catch(err => console.error(err));
